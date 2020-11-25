2d64fe5acfc8509316c300fe944604a0632cdea3
Jonathan Yeong <jonathan.yeong@highspot.com>
2020-11-24 19:44:07 -0800

    Finish adding steep files
    Add a test for typing

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require "./inventory_app/item"
 require "./inventory_app/storefront"
 
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
 s = InventoryApp::Storefront.new(name: "Hello world", location: "Seattle", owner: "Jono", template_store: :grocery)
 
 puts s.inspect
 
 puts "Items in store: #{s.items}"
+
+# s1 = InventoryApp::Storefront.new(name: 1234, location: :seattle, owner: Person.new("Jono"), template_store: :grocery)
